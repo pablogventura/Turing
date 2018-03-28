53f1d8e43088e2b469aa7ab6549fa7847b54ab33
Pablo Ventura <pablo@claude.workgroup>
2018-03-28 11:30:00 -0300

    cintas

--- tm.py
+++ tm.py
@@ -1,3 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+playSym = "▷"
+blankSym = "□"
+
+def warning(s):
+    print("WARNING: %s" % s)
+
+class Cinta(object):
+    """
+    Objeto para representar cintas infintas
+    
+    >>> c = Cinta([e for e in "hola"])
+    >>> c
+    (▷, hola)
+    """
+    def __init__(self,x=[],solo_lectura=False):
+        self.u=[playSym]
+        self.v=x
+        self.solo_lectura=solo_lectura
+    def leer(self):
+        return self.u[-1]
+    def escribir(self,sym):
+        if not self.solo_lectura:
+            self.u[-1] = sym
+        else:
+            warning("Intento de escritura en una cinta de solo lectura")
+    def left(self):
+        if not len(self.u) <= 1:
+            self.v.insert(0,self.u[-1])
+            del self.u[-1]
+        self._clean()
+    def right(self):
+        if self.v:
+            self.u.append(self.v[0])
+            del self.v[0]
+        else:
+            self.u.append(blankSym)
+        self._clean()
+    def _clean(self):
+        while self.v and self.v[-1]==blankSym:
+            del self.v[-1]
+    def __repr__(self):
+        if self.v:
+            return "("+''.join(self.u) +", " + ''.join(self.v)+")"
+        else:
+            return "("+''.join(self.u) +", ε)"
+
 class TM(object):
     """
     Clase para las maquinas de turing
@@ -9,5 +58,5 @@ class TM(object):
     def correr(self):
         while not self.is_halted():
             self.avanzar()
-    def is_halted(self):
+    def paro(self):
         pass
